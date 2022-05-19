8aa1be227825d74674794126b218db2fb0a7a975
Leonard Constant <leonardconstant1995@gmail.com>
2022-05-19 14:37:50 -0400

    8 kyu

---
+++ codewar3.js
@@ -0,0 +1,50 @@
+/*Task
+Please refer to two example above and write your first JS function.
+
+mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line(don't forget to put the str in the brackets).
+
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+In the end, click "Submit" to submit your code pass this kata.*/
+
+
+
+
+
+var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
+function equal1(){
+  var a=v1   
+  var b=v1   
+  return a+b;
+}
+//Please refer to the example above to complete the following functions
+function equal2(){
+  var a=  v3
+  var b=  v1
+  return a-b;
+}
+function equal3(){
+  var a=  v1
+  var b=  v5
+  return a*b;
+}
+function equal4(){
+  var a=   v4
+  var b=   v5
+  return a/b;
+}
+function equal5(){
+  var a=   v2
+  var b=   v6
+  return a%b;
+}
