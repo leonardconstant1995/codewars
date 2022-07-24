1290c768cdf11a8cec01343cc42c9dad8900ae8d
Leonard Constant <leonardconstant1995@gmail.com>
2022-07-24 01:58:28 -0400

    8kyu

--- 8kyu/AreyouPlayingBanjo.js
+++ 8kyu/AreyouPlayingBanjo.js
@@ -3,7 +3,6 @@ If your name starts with the letter "R" or lower case "r", you are playing banjo
 
 The function takes a name as its only argument, and returns one of the following strings:*/
 
-
 function areYouPlayingBanjo(name) {
     if(name.charAt(0) === 'R' || name.charAt(0) === 'r'){
       return `${name} plays banjo`
